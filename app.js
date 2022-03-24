20a8882cde3f119b846fb96b78ba642a119df71c
doeej782 <doeej782@gmail.com>
2022-03-24 18:03:45 +0500

    header bg

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
-let x = 0;
+let burger = document.querySelector(".burger")
+let burger_modal = document.querySelector(".burger_modal")
 
+burger.addEventListener("click" ,  () => {
 
+    burger_modal.classList.toggle("open_modal")
+   
+})
+
+// car modal
+
+let car_modal = document.querySelector(".car_modal")
+let cars__car = document.querySelectorAll(".cars__car")
+let close_car = document.querySelector(".close_car")
+cars__car.forEach(item => {
+    item.addEventListener("click", ()=>{
+
+        
+        car_modal.classList.add("car_modal_open")
+    })
+})
+
+close_car.addEventListener("click", ()=> {
+
+    car_modal.classList.remove("car_modal_open")
+
+})
 
 
 
